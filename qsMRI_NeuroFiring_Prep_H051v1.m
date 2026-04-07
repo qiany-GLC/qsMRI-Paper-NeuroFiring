29a62c8d28596ac33ba010bd277b94622e741082
Yongxian Qian <yongxian_qian@yahoo.com>
2026-04-07 16:01:19 -0400

    Update qsMRI_NeuroFiring_Prep_H051v1.m

--- qsMRI_NeuroFiring_Prep_H051v1.m
+++ qsMRI_NeuroFiring_Prep_H051v1.m
@@ -11,6 +11,7 @@
 %    TRms -- repetition time, ms 
 % ----------------------------------------------------------------------
 % 2026-04-02 yxq cleaned for Nature's reviewers
+% 2026-04-07 yxq split a big data into two, then Concatenate _part1 and _part2
 %
 % -------------------------------------------------------------------------
 
@@ -24,8 +25,14 @@ param.myColor = '-k';        % color code for qsMRI plot: black
 param.fidRange = [59,3,61];  % [stFid,increment,endFid] to plot
 
 % === Tap ===
-load ('fidDataH051v1HN20_Tap','fidDataH051v1HN20_Tap'); % load raw FID data: H051v1 Head/Neck 20 channels 
-                                    % [NCol,NCha,NScans]=[4096,20,64]
+    % load raw FID data: H051v1 Head/Neck 20 channels 
+    % [NCol,NCha,NScans]=[4096,20,64]
+                                    
+% load ('fidDataH051v1HN20_Tap','fidDataH051v1HN20_Tap'); 
+
+load ('fidDataH051v1HN20_Tap_part1','fidDataH051v1HN20_Tap_part1');
+load ('fidDataH051v1HN20_Tap_part2','fidDataH051v1HN20_Tap_part2');
+fidDataH051v1HN20_Tap = cat(3, fidDataH051v1HN20_Tap_part1,fidDataH051v1HN20_Tap_part2);
 
 param.fidData = fidDataH051v1HN20_Tap;   % FID raw data w/ complex valued data points
                                         % with 20 Head/Neck Channels
@@ -33,14 +40,22 @@ param.fidData = fidDataH051v1HN20_Tap;   % FID raw data w/ complex valued data p
 qsMRI_NeuroFiring_Verify(param);
 
 % === noTap ===
-load ('fidDataH051v1HN20_noTap','fidDataH051v1HN20_noTap');
+% load ('fidDataH051v1HN20_noTap','fidDataH051v1HN20_noTap');
+
+load ('fidDataH051v1HN20_noTap_part1','fidDataH051v1HN20_noTap_part1');
+load ('fidDataH051v1HN20_noTap_part2','fidDataH051v1HN20_noTap_part2');
+fidDataH051v1HN20_noTap = cat(3, fidDataH051v1HN20_noTap_part1,fidDataH051v1HN20_noTap_part2);
 
 param.fidData = fidDataH051v1HN20_noTap;
 
 qsMRI_NeuroFiring_Verify(param);
 
 % === Rest ===
-load ('fidDataH051v1HN20_Rest','fidDataH051v1HN20_Rest');
+% load ('fidDataH051v1HN20_Rest','fidDataH051v1HN20_Rest');
+
+load ('fidDataH051v1HN20_Rest_part1','fidDataH051v1HN20_Rest_part1');
+load ('fidDataH051v1HN20_Rest_part2','fidDataH051v1HN20_Rest_part2');
+fidDataH051v1HN20_Rest = cat(3, fidDataH051v1HN20_Rest_part1,fidDataH051v1HN20_Rest_part2);
 
 param.fidData = fidDataH051v1HN20_Rest;
 
